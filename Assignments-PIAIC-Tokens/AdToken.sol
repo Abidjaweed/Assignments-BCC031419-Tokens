2e8614b756d6e14d1bdb6a0a03c02881080d30ad
Abid Jaweed <52130668+Abidjaweed@users.noreply.github.com>
2020-05-20 14:06:54 -0700

    Add files via upload

---
+++ Assignments-PIAIC-Tokens/AdToken.sol
@@ -0,0 +1,150 @@
+pragma solidity ^0.6.0;
+
+/**
+* Token Contract Address: 0x93d8f72252fc3a69bc4cd8eef2f171e7850c0917
+* Ropsten Token Deployed at:https://ropsten.etherscan.io/token/0x93d8f72252fc3a69bc4cd8eef2f171e7850c0917
+*
+* Token Contract Address: 0x366b7af496aa923e724d0680264018d3f1244a0e
+* Token Contract Address: https://ropsten.etherscan.io/token/0x366b7af496aa923e724d0680264018d3f1244a0e
+*/
+
+import "./IERC20.sol";
+
+contract AdToken is IERC20{
+    //mapping to hold balances against EOA accounts
+    mapping (address => uint256) private _balances;
+
+    //mapping to hold approved allowance of token to certain address
+    //       Owner               Spender    allowance
+    mapping (address => mapping (address => uint256)) private _allowances;
+
+    //the amount of tokens in existence
+    uint256 private _totalSupply;
+
+    //owner
+    address public owner;
+    
+    string public name;
+    string public symbol;
+    uint8 public decimals;
+
+    constructor () public {
+        name = "Abid Practice Token 1";
+        symbol = "AdT1";
+        decimals = 18;
+        owner = msg.sender;
+        
+        //1 million tokens to be generated
+        _totalSupply = 1000000 * 10**uint256(decimals);
+        //transfer total supply to owner
+        _balances[owner] = _totalSupply;
+        
+        //fire an event on transfer of tokens
+        emit Transfer(address(this),owner,_totalSupply);
+     }
+
+    /**
+     * @dev See {IERC20-totalSupply}.
+     */
+    function totalSupply() public view override returns (uint256) {
+        return _totalSupply;
+    }
+
+    /**
+     * @dev See {IERC20-balanceOf}.
+     */
+    function balanceOf(address account) public view override returns (uint256) {
+        return _balances[account];
+    }
+
+    /**
+     * @dev See {IERC20-transfer}.
+     *
+     * Requirements:
+     *
+     * - `recipient` cannot be the zero address.
+     * - the caller must have a balance of at least `amount`.
+     */
+    function transfer(address recipient, uint256 amount) public virtual override returns (bool) {
+        address sender = msg.sender;
+        require(sender != address(0), "AdT1: transfer from the zero address");
+        require(recipient != address(0), "AdT1: transfer to the zero address");
+        require(_balances[sender] > amount,"AdT1: transfer amount exceeds balance");
+
+        //decrease the balance of token sender account
+        _balances[sender] = _balances[sender] - amount;
+        
+        //increase the balance of token recipient account
+        _balances[recipient] = _balances[recipient] + amount;
+
+        emit Transfer(sender, recipient, amount);
+        return true;
+    }
+
+    /**
+     * @dev See {IERC20-allowance}.
+     */
+    function allowance(address tokenOwner, address spender) public view virtual override returns (uint256) {
+        return _allowances[tokenOwner][spender];
+    }
+
+    /**
+     * @dev See {IERC20-approve}.
+     *
+     * Requirements:
+     *
+     * - `spender` cannot be the zero address.
+     */
+    function approve(address spender, uint256 amount) public virtual override returns (bool) {
+        address tokenOwner = msg.sender;
+        require(tokenOwner != address(0), "AdT1: approve from the zero address");
+        require(spender != address(0), "AdT1: approve to the zero address");
+        
+        _allowances[tokenOwner][spender] = amount;
+        
+        emit Approval(tokenOwner, spender, amount);
+        return true;
+    }
+
+    /**
+     * @dev See {IERC20-transferFrom}.
+     *
+     * Emits an {Approval} event indicating the updated allowance. This is not
+     * required by the EIP. See the note at the beginning of {ERC20};
+     *
+     * Requirements:
+     * - `sender` and `recipient` cannot be the zero address.
+     * - `sender` must have a balance of at least `amount`.
+     * - the caller must have allowance for ``sender``'s tokens of at least
+     * `amount`.
+     */
+    function transferFrom(address tokenOwner, address recipient, uint256 amount) public virtual override returns (bool) {
+        address spender = msg.sender;
+        uint256 _allowance = _allowances[tokenOwner][spender];
+        require(_allowance > amount, "AdT1: transfer amount exceeds allowance");
+        
+        //deducting allowance
+        _allowance = _allowance - amount;
+        
+        //--- start transfer execution -- 
+        
+        //owner decrease balance
+        _balances[tokenOwner] =_balances[tokenOwner] - amount; 
+        
+        //transfer token to recipient;
+        _balances[recipient] = _balances[recipient] + amount;
+        
+        emit Transfer(tokenOwner, recipient, amount);
+        //-- end transfer execution--
+        
+        //decrease the approval amount;
+        _allowances[tokenOwner][spender] = _allowance;
+        
+        emit Approval(tokenOwner, spender, amount);
+        
+        return true;
+    }
+    
+
+}
+
